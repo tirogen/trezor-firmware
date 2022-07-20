2473f19e4110c3c49f12fe25c72a8904a6e61e8f
matejcik <ja@matejcik.cz>
2022-07-20 14:54:19 +0200

    wip: fix removed TT/T1 files

--- core/src/trezor/ui/layouts/__init__.py
+++ core/src/trezor/ui/layouts/__init__.py
@@ -6,13 +6,10 @@ from .common import UI2
 # NOTE: using any import magic probably causes mypy not to check equivalence of
 #       layout type signatures across models
 if utils.MODEL in ("1",):
-    from .t1 import *  # noqa: F401,F403
+    raise NotImplementedError
 elif utils.MODEL in ("R",):
     from .tr import *  # noqa: F401,F403
 elif utils.MODEL in ("T",):
-    if not UI2:
-        from .tt import *  # noqa: F401,F403
-    else:
-        from .tt_v2 import *  # noqa: F401,F403
+    from .tt_v2 import *  # noqa: F401,F403
 else:
     raise ValueError("Unknown Trezor model")
