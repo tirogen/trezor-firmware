067501d0a49a63506064bb187deac61d632f2aeb
grdddj <jiri.musil06@seznam.cz>
2023-01-11 12:31:32 +0100

    WIP - create confirm_blob with not showing hyphens and line-ends

--- core/src/trezor/ui/layouts/tt_v2/__init__.py
+++ core/src/trezor/ui/layouts/tt_v2/__init__.py
@@ -337,7 +337,7 @@ async def confirm_path_warning(
 
 
 def _show_xpub(xpub: str, title: str, cancel: str | None) -> ui.Layout:
-    content = RustLayout(
+    return RustLayout(
         trezorui2.confirm_blob(
             title=title,
             data=xpub,
@@ -346,7 +346,6 @@ def _show_xpub(xpub: str, title: str, cancel: str | None) -> ui.Layout:
             description=None,
         )
     )
-    return content
 
 
 async def show_xpub(ctx: GenericContext, xpub: str, title: str) -> None:
