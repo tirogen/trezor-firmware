17d317e767eb0a1711fed14c0f305b2c8c086e9c
tychovrahe <brunam@seznam.cz>
2023-01-13 10:15:35 +0100

    fixup! feat(core): implement loaders for T2B1

--- core/embed/rust/src/ui/model_t1/constant.rs
+++ core/embed/rust/src/ui/model_t1/constant.rs
@@ -5,6 +5,10 @@ pub const HEIGHT: i16 = 64;
 pub const LINE_SPACE: i16 = 1;
 pub const FONT_BPP: i16 = 1;
 
+pub const LOADER_OUTER: f32 = 32_f32;
+pub const LOADER_INNER: f32 = 18_f32;
+pub const LOADER_ICON_MAX_SIZE: i16 = 8;
+
 pub const fn size() -> Offset {
     Offset::new(WIDTH, HEIGHT)
 }
